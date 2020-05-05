06f53c314440f3df42e3bd4975d07452c37bbd76
Senada Kadric <skadric@fordham.edu>
2020-05-05 23:23:01 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,21 @@
 require_relative './part_1_solution.rb'
+require 'pry'
+
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
+  # cart is an array of item hashes and coupons is an array of coupon hashes
+  
   # REMEMBER: This method **should** update cart
+  
+
+coupons.each do |coupon_hash|
+    
+    #checking if the cart has a value corresponding to the value of the item of the coupon
+    if cart.has_value?(coupon_hash[:item])
+      
+    end 
+  end
+ 
 end
 
 def apply_clearance(cart)
